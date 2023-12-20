13ea97456b48f86097c53308a5ae93ab6829c5a0
danielgoss22 <daniel.goss22@gmail.com>
2023-12-20 09:10:51 -0700

    added requires for inquirer, fs, and shapes.js and initial propmt array

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const shapes = require("./lib/shapes");
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What are your Initials?",
+        name: "initials",
+      },
+      {
+        type: "input",
+        message: "What Color would you like your custom logo to be?",
+        name: "color",
+      },
+      {
+        type: "list",
+        message: "What Shape would you like you logo to be?",
+        choices: ["Circle", "Square", "Triangle"],
+        name: "shape",
+      },
+    ])
+    .then(function (userInput) {
+      const shapesTemplate = generateLogo(userInput);
+      fs.writeFile("logo.svg", shapesTemplate, function (error) {
+        error
+          ? console.log("An error has taken place.")
+          : console.log("Your Logo has been created!");
+      });
+    });
+}
+init();
